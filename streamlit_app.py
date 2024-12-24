aa2ef633e7a168b9d390801c02005ab12a4c0950
Bianca Navey <74916027+biancanavey@users.noreply.github.com>
2024-12-24 12:23:34 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,8 +48,9 @@ if time_to_insert:
     st.success('Your Smoothie is ordered, ' + name_on_order + '!' , icon="✅")
 
 # New section to display smoothiefroot nutrition information
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 
-# st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response.json())
+
+# sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
